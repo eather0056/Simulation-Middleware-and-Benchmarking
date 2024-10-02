4d4a12332cfa3900155920d3844eefa41993b5ba
eather0056 <mdether0056@gmail.com>
2024-10-02 10:16:30 +0200

    Version 4

--- activity_2/Sum/sum.cc
+++ activity_2/Sum/sum.cc
@@ -7,12 +7,14 @@ int main() {
     std::cout << "Enter a number: ";
     std::cin >> number;
 
-    for (int i = 1; i <= number; ++i) {
-        sum += i;
+    if (number >= 1) {
+        for (int i = 1; i <= number; ++i) {
+            sum += i;
+        }
+        std::cout << "The sum of 1 to " << number << " is " << sum << "." << std::endl;
+    } else {
+        std::cout << "Number must be greater than or equal to 1." << std::endl;
     }
-
-    std::cout << "The sum of 1 to " << number << " is " << sum << "." << std::endl;
 
     return 0;
 }
-
