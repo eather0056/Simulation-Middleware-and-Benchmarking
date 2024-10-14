75303e983c452721af793039c6760aaaed74782a
eather0056 <mdether0056@gmail.com>
2024-10-14 17:36:08 +0200

    ros_sum version 4

--- activity_3/ros2_ws/src/sum/src/sum.cpp
+++ activity_3/ros2_ws/src/sum/src/sum.cpp
@@ -1,22 +1,32 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
-//#include "std_msgs/msg/string.hpp"
 
 class SumNode : public rclcpp::Node {
 public:
-    SumNode() : Node("sum_node") {
+    SumNode() : Node("sum_node"), sum_(0) {
         subscription_ = this->create_subscription<std_msgs::msg::Int32>(
             "input_topic", 10, std::bind(&SumNode::topic_callback, this, std::placeholders::_1));
+
+        // Publisher can be added in Version 5
+        // publisher_ = this->create_publisher<std_msgs::msg::Int32>("output_topic", 10);
     }
 
 private:
     void topic_callback(const std_msgs::msg::Int32::SharedPtr msg) {
-        RCLCPP_INFO(this->get_logger(), "I heard: '%d'", msg->data);
+        sum_ += msg->data;
+        RCLCPP_INFO(this->get_logger(), "Sum: '%d'", sum_);
+        
+        // For Version 5, to publish sum
+        // auto message = std_msgs::msg::Int32();
+        // message.data = sum_;
+        // publisher_->publish(message);
     }
 
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr subscription_;
+    // Add a publisher for Version 5
+    // rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
+    int sum_;
 };
-
 
 int main(int argc, char * argv[]) {
     rclcpp::init(argc, argv);
